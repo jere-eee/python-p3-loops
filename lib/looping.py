375651a55059e4229464303822094b56ebfa1240
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-25 15:41:08 +0300

    looping

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [nu**2 for nu in int_list]
 
 def fizzbuzz():
     # code goes here!
-    pass
+    num = 1
+    while num < 101:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0 and num % 5 != 0:
+            print("Fizz")
+        elif num % 3 != 0 and num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
